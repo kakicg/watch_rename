1c20b3258d6b8b71f1cde50ffe8dda4826fed817
柿山幹雄 <kakicg@cgkojo.com>
2024-01-11 17:57:49 +0900

    send_warning

--- main.js
+++ main.js
@@ -255,6 +255,7 @@ watcher.on('ready',function(){
                         eventLogger.info(`フォトデータ: ${photo.name} ${photo.date}`);            
                     } else {
                         sys.remove_file( watch_dir + "/" + new_name );
+                        send_warning("バーコード情報がありません", message, 1)
                     }
                 } else {
                     photo.date = new Date();
